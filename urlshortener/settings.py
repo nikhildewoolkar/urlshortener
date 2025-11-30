58213fb3865c995c3af2208708f6d2223648f5be
nikhildewoolkar <dewoolkarnikhil29@gmail.com>
2025-11-30 17:32:19 +0530

    docker-publish fixed

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -17,7 +17,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
